ab6f4ee78ab49f81c69f8818c2109b71518c60e7
JNDiaz8 <33089232+JNDiaz8@users.noreply.github.com>
2017-10-26 13:22:09 +0200

    update HolaMundo.java
    El saludo se hace desde un metodo aparte

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,8 @@
 public class HolaMundo{
  public static void main (String [] args){
-  System.out.println("Hola mundo");
+  
+  }
+ public static void saludar{
+  System.out.println("Hola Mundo")
   }
  }
